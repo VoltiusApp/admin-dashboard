7e1a6e948d680eecd7d94b766f612f5c238de379
kipavy <kiki.kalagan@gmail.com>
2026-04-22 18:31:48 +0200

    fix

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,9 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === "/admin/login") {
+    return NextResponse.next();
+  }
   const session = request.cookies.get("ADMIN_SESSION");
   if (!session?.value) {
     return NextResponse.redirect(new URL("/admin/login", request.url));
